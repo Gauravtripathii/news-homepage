99a0b496713e5e901c287155c50a7c4ec166dcab
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-11-25 20:48:13 +0530

    static texts

---
+++ src/static/index.js
@@ -0,0 +1,65 @@
+export const active = {
+    title: "The Bright Future of Web 3.0?",
+    para: "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of people. But is it really fullfilling its promise?",
+    button_text: "READ MORE",
+}
+
+// export const nav = ["Home", "New", "Popular", "Trending", "Categories"]
+export const nav = [
+  {
+    id: 0,
+    name: "Home",
+  },
+  {
+    id: 1,
+    name: "New",
+  },
+  {
+    id: 2,
+    name: "Popular",
+  },
+  {
+    id: 3,
+    name: "Trending",
+  },
+  {
+    id: 4,
+    name: "Categories",
+  },
+];
+
+export const new_news = [
+  {
+    id: 0,
+    title: "Hydrogen VS Electric Cars",
+    para: "Will hydrogen fuled cars ever catch up to EVs?",
+  },
+  {
+    id: 1,
+    title: "The Downside of AI artristry",
+    para: "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    id: 2,
+    title: "Is VC funding drying up?",
+    para: "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+  },
+];
+
+export const news_flash = [
+    {
+        id: 01,
+        title: "Reviving Retro PCs",
+        para: "What happens when old PCs are given modern upggrades?",
+    },
+    {
+        id: 02,
+        title: "Top 10 Laptops of 2022",
+        para: "Our best piicks for various needs and budgets.",
+    },
+    {
+        id: 03,
+        title: "The Growth of Gaming",
+        para: "How the panndemic has sparked fresh oppertunities.",
+    },
+]
