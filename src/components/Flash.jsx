c53c4ea28bd654e4303b6a5b4317554dd41a2262
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-12-15 11:53:14 +0530

    flash - mobile view

--- src/components/Flash.jsx
+++ src/components/Flash.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import "./flash.css";
+import { news_flash } from "../static";
+import im1 from "../assets/images/image-retro-pcs.jpg"
+import im2 from "../assets/images/image-top-laptops.jpg"
+import im3 from "../assets/images/image-gaming-growth.jpg"
 
 function Flash() {
   return (
-    <div>Flash</div>
-  )
+    <div className="flash">
+      {news_flash.map((nf) => (
+        <div className="card" key={nf.id}>
+          <div className="image">
+            <img src={nf.id === 1? im1 : nf.id === 2? im2 : im3} alt="img" />
+          </div>
+          <div className="right">
+            <div className="no">0{nf.id}</div>
+            <div className="title">{nf.title}</div>
+            <div className="para">{nf.para}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Flash
+export default Flash;
