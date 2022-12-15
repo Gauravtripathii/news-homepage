3be076872a6f5cf2b94fe636974887e049e7ed0f
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-12-15 09:58:22 +0530

    top and home full responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import New from "./components/New";
-// import Flash from "./components/Flash";
+import Flash from "./components/Flash";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Home />
-      {/* <New /> */}
-      {/* <Flash /> */}
+      <div className="top">
+        <Home />
+        <New />
+      </div>
+      <Flash />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,15 @@ import { active } from "../static";
 
 function Home() {
   return (
-    <div className='home'>
+    <div className="home">
       <div className="profile-image"></div>
-      <div className="title">
-        {active.title}
-      </div>
-      <div className="para">
-        {active.para}
-      </div>
-      <div className="button">
-        {active.button_text}
+      <div className="title">{active.title}</div>
+      <div className="right">
+        <div className="para">{active.para}</div>
+        <div className="button">{active.button_text}</div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
